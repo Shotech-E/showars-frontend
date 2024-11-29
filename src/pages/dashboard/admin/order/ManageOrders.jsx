bb184d959c72d4f491cae6fa7ed2acc9aa07082f
Shotech <40011872+Shotech-E@users.noreply.github.com>
2024-11-29 13:09:36 +0100

    Update ManageOrders.jsx

--- src/pages/dashboard/admin/order/ManageOrders.jsx
+++ src/pages/dashboard/admin/order/ManageOrders.jsx
@@ -36,60 +36,143 @@ const ManageOrders = () => {
 	if (isLoading) return <div>Loading....</div>
 	if (error) return <div>Something went wrong!</div>
 
-	return (
-    <div className="section__container p-6">
-      <h2 className="text-2xl font-semibold mb-4">Manage Orders</h2>
-      <table className="min-w-full bg-white border border-gray-200 rounded-lg">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="py-3 px-4 border-b">Order Id</th>
-            <th className="py-3 px-4 border-b">Customer Email</th>
-            <th className="py-3 px-4 border-b">Status</th>
-            <th className="py-3 px-4 border-b">Date</th>
-            <th className="py-3 px-4 border-b">Actions</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {orders &&
-            orders.map((order, index) => (
-              <tr key={index}>
-                <td className="py-3 px-4 border-b">{order?.orderId}</td>
-                <td className="py-3 px-4 border-b">{order?.email}</td>
-                <td className="py-3 px-4 border-b">
-                  <span
-                    className={`inline-block px-3 py-1 text-xs text-white rounded-full ${getStatusColor(
-                      order?.status
-                    )}`}
-                  >
-                    {order?.status}
-                  </span>
-                </td>
-                <td className="py-3 px-4 border-b">
-                  {formatDate(order?.updatedAt)}
-                </td>
-                <td className="py-3 px-4 border-b flex items-center space-x-4">
-                  <Link to="#" className="text-blue-500 hover:underline">
-                    <i className="ri-eye-line mr-2 text-yellow-500 hover:text-yellow-900"></i>
-                  </Link>
+  return (
+    <>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error loading users data.</div>}
+      <section className="py-1 bg-blueGray-50">
+        <div className="w-full  mb-12 xl:mb-0 px-4 mx-auto">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+              <div className="flex flex-wrap items-center">
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 className="font-semibold text-base text-blueGray-700">
+                    Manage Orders
+                  </h3>
+                </div>
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
                   <button
-                    className="text-green-500 hover:underline"
-                    onClick={() => handleEditOrder(order)}
+                    className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
                   >
-                    <i className="ri-edit-box-line mr-2 text-green-500 hover:text-green-900"></i>
+                    See all
                   </button>
-                  <button
-                    className="text-red-500 hover:underline"
-                    onClick={() => handleDeleteOder(order?._id)}
-                  >
-                    <i className="ri-delete-bin-2-line text-red-500 hover:text-red-900"></i>
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                </div>
+              </div>
+            </div>
 
+            <div className="block w-full overflow-x-auto">
+              <table className="items-center bg-transparent w-full border-collapse">
+                <thead>
+                  <tr>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      No.
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Order Id
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Customer Email
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Status
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      Date
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                      {" "}
+                      Actions{" "}
+                    </th>{" "}
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {orders &&
+                    orders.map((order, index) => (
+                      <tr key={index}>
+                        <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                          {index + 1}
+                        </th>
+                        <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                          <img
+                            className="w-full h-full rounded-full"
+                            src={order.image}
+                            alt=""
+                          />{" "}
+                          {order?.orderId}
+                        </td>
+                        <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          {order?.email}
+                        </td>
+                        <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          <span
+                            className={`inline-block px-3 py-1 text-xs text-white rounded-full ${getStatusColor(
+                              order?.status
+                            )}`}
+                          >
+                            {order?.status}
+                          </span>
+                        </td>
+                        <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                          {formatDate(order?.updatedAt)}
+                        </td>
+                        <td className="border-b-0 border-gray-200 bg-white text-sm">
+                          <Link
+                            to="#"
+                            className="text-blue-500 hover:underline"
+                          >
+                            <i className="ri-eye-line p-2 text-yellow-500 hover:text-yellow-900"></i>
+                          </Link>
+                          <button
+                            className="text-green-500 hover:underline"
+                            onClick={() => handleEditOrder(order)}
+                          >
+                            <i className="ri-edit-box-line p-2 text-green-500 hover:text-green-900"></i>
+                          </button>
+                          <button
+                            className="text-red-500 hover:underline"
+                            onClick={() => handleDeleteOder(order?._id)}
+                          >
+                            <i className="ri-delete-bin-2-line p-2 text-red-500 hover:text-red-900"></i>
+                          </button>
+                        </td>{" "}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <footer className="relative pt-8 pb-6 mt-16">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap items-center md:justify-between justify-center">
+              <div className="w-full md:w-6/12 px-4 mx-auto text-center">
+                <div className="text-sm text-blueGray-500 font-semibold py-1">
+                  Made with{" "}
+                  <a
+                    href="https://www.creative-tim.com/product/notus-js"
+                    className="text-blueGray-500 hover:text-gray-800"
+                    target="_blank"
+                  >
+                    Shotech Enterprises
+                  </a>{" "}
+                  by{" "}
+                  <a
+                    href="https://www.creative-tim.com"
+                    className="text-blueGray-500 hover:text-blueGray-800"
+                    target="_blank"
+                  >
+                    {" "}
+                    Shotech
+                  </a>
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </section>
       {/* update order modal */}
       {selectedOrder && (
         <UpdateOrderModal
@@ -98,7 +181,7 @@ const ManageOrders = () => {
           onClose={handleCloseModal}
         />
       )}
-    </div>
+    </>
   );
 }
 
